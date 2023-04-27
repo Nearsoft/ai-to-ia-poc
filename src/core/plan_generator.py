a4b56ef5a56b5cdbe89dc2acc20936b3728461ff
Jorge Hernandez <jd.hernandez@gmail.com>
2023-04-26 18:20:25 -0600

    Added docstrings and comments to `plan_generator.py`.

--- src/core/plan_generator.py
+++ src/core/plan_generator.py
@@ -1,17 +1,30 @@
+"""PlanGenerator module. Does plan generation for the `Orchestrator`."""
 
+from typing import Dict
 from metadata import Metadata
 
 class PlanGenerator:
-    def __init__(self, metadata):
-        self.metadata = metadata
-        self.plan = self.generate_plan()
+    """
+    Class to perform plan generation based upon available input and metadata.
+    
+    Attributes
+    ----------
+    metadata :: (Metadata): Metadata for the plan.
+    plan :: (Dict[str, object]): Generated plan for the orchestrator to follow.
+    """
 
-    def generate_plan(self):
+    def __init__(self, metadata: Metadata):
+        self.metadata = metadata
+        self.plan = self._generate_plan()
+
+    def _generate_plan(self) -> Dict[str, object]:
+        """Generates a plan for the model to follow."""
         plan = {
             "original_question": "", 
             "image": None,
             "optional_responses": [],
             "metadata": self.metadata,
-            "model_sequence": []
+            "model_sequence": []  # this is a List[AbstractModel->Model].
         }
+
         return plan
